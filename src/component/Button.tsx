79fd37e37679d18fd13c685e82c669c23d8a322f
xiaoquanquanxin <xxkl123@126.com>
2021-03-05 00:07:08 +0800

    第一次构建

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 function Button({color}: { color: string }) {
 	console.log(color);
 	return (
-		<div>我是按钮 {color}</div>
+		<div style={{color: color}}>我是按钮 {color}</div>
 	);
 }
 
