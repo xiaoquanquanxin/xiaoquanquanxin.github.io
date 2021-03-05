9400fdec975d6bb88e735bde96f73dd699b32284
quanxin <quanxin@pujitech.com>
2021-03-05 18:04:00 +0800

    github fork

---
+++ src/components/global/gitubFork/gitubFork.tsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useRef} from "react";
+import a from '@img/global/forkme.png'
+
+import "./gitubFork.less";
+
+
+export const GithubFork = () => {
+    return (
+        <a className='github-fork' href="https://github.com/xiaoquanquanxin" target="_blank" rel='noreferrer'>
+            <img src="https://chokcoco.github.io/demo/curveJS/images/forkme.png"
+                 alt={'https://github.com/xiaoquanquanxin'}/>
+        </a>
+    )
+};
+
+
+
+
+
