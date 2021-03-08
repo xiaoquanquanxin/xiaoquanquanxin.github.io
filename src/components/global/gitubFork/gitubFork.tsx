fe997a7ae5a7ea7905bc4a4c8ba96cc68f74778b
quanxin <quanxin@pujitech.com>
2021-03-08 16:03:45 +0800

    fork

--- src/components/global/gitubFork/gitubFork.tsx
+++ src/components/global/gitubFork/gitubFork.tsx
@@ -2,10 +2,11 @@ import React, {useEffect, useRef} from "react";
 import style from "./gitubFork.module.less";
 import forkme from '@img/global/forkme.png'
 
+const href = 'https://github.com/xiaoquanquanxin';
 export const GithubFork = () => {
     return (
-        <a className={style.githubFork} href="https://github.com/xiaoquanquanxin" target="_blank" rel='noreferrer'>
-            <img src={forkme} alt={'https://github.com/xiaoquanquanxin'}/>
+        <a className={style.githubFork} href={href} target="_blank" rel='noreferrer'>
+            <img src={forkme} alt={href}/>
         </a>
     )
 };
