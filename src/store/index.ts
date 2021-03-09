36f3c698e783e2ffcd80d0d4f98e7e3769c371f5
xiaoquanquanxin <xxkl123@126.com>
2021-03-09 22:39:27 +0800

    计算

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,26 +2,38 @@ import {computed, action, makeAutoObservable} from "mobx";
 import {SliderDataModel, SliderListDataModel} from "@models/slider";
 
 export class Store {
-    constructor() {
-        makeAutoObservable(this)
-    }
+	constructor() {
+		makeAutoObservable(this)
+	}
 
-    //  菜单数据
-    private sliderData: Array<SliderDataModel> = [];
+	//  菜单数据
+	private sliderListData: Array<SliderDataModel> = [];
 
-    @computed get getSliderData() {
-        console.log('读取');
-        return this.sliderData;
-    }
+	@computed get getSliderListData() {
+		return this.sliderListData;
+	}
 
-    @action
-    setSliderListData = (sliderData: SliderListDataModel) => {
-        this.sliderData = sliderData;
-    };
+	@action
+	setSliderListData = (sliderListData: SliderListDataModel) => {
+		this.sliderListData = sliderListData;
+	};
+
+	//  路由数据
+	private routerListData: Array<SliderDataModel> = [];
+
+	@computed get getRouterListData() {
+		return this.routerListData;
+	}
+
+	@action
+	setRouterListData = (routerListData: SliderListDataModel) => {
+		this.routerListData = routerListData;
+	};
+
 }
 
 
 const store = new Store();
 export {
-    store,
+	store,
 }
