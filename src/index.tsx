cbd56852494ccbb343754927b18aff6671a31287
quanxin <quanxin@pujitech.com>
2021-03-09 16:44:14 +0800

    注入mobx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
+import {Provider} from 'mobx-react';
+import {store, actions} from "@store/index";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App/>
-	</React.StrictMode>,
-	document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store} actions={actions}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
