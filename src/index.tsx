e0c3a0ad900beb160965c742371ba17c942f0cfc
quanxin <quanxin@pujitech.com>
2021-03-10 16:47:43 +0800

    高亮

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
 import App from './App';
 import {Provider} from 'mobx-react';
 import {store} from "@store/index";
 import reportWebVitals from './reportWebVitals';
+import './index.less';
+import '@css/androidstudio.css';
+import '@css/androidstudio.font.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
