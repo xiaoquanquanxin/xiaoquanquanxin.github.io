cfb99a2c7cc45936ded85f5a43176b95288cb4e4
quanxin <quanxin@pujitech.com>
2021-03-10 17:29:53 +0800

    占位符

---
+++ src/components/highLightPlaceholder/highLightPlaceholder.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from './highLightPlaceholder.module.less';
+
+//	高亮的占位符
+export const highLightPlaceholder = () => <pre className={style.highLightPlaceholder}/>;
+
+
+
