e0ff23b2ce426dda29733172a5fef5a23ef524f5
quanxin <quanxin@pujitech.com>
2021-03-10 15:29:38 +0800

    loading

--- src/store/index.ts
+++ src/store/index.ts
@@ -32,15 +32,60 @@ export class Store {
 		this.routerListData = routerListData;
 	};
 
-	//  路由数据
-	private loading: boolean = false;
+	//  loading...
+	private loading: boolean = true;
+
+	@computed
+	get getLoading() {
+		return this.loading;
+	}
+
+	//	正在延迟中？
+	private isDelay: boolean = true;
+	//	定时器
+	private loadingTimeout: number = 0;
+
+	@action
+	setLoadingTimeout(loadingTimeout) {
+		this.loadingTimeout = loadingTimeout;
+	}
+
 	@action
 	setLoading = (loading: boolean) => {
-		this.loading = loading;
+		//	如果不在延迟中，或者打开loading，那么无脑设置loading
+		if (!this.isDelay || loading) {
+			this.loading = loading;
+			this.isDelay = true;
+			clearTimeout(this.loadingTimeout);
+			return;
+		}
+		this.setLoadingTimeout(window.setTimeout(() => {
+			this.isDelay = false;
+			if (!loading) {
+				this.loading = loading;
+				return;
+			}
+			this.setLoading(loading);
+		}, 200))
+	};
+
+	//	文章数据
+	private articleData: Object = {};
+
+	@computed
+	get getArticleData() {
+		return this.articleData;
+	}
+
+	@action
+	setArticleData = (key: string, value: Object) => {
+		this.articleData = {
+			...this.articleData,
+			[key]: value,
+		};
 	};
 
 }
-
 
 const store = new Store();
 export {
