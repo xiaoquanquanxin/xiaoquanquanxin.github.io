c4f5ced887b55fce56b4ca53c656355eeaa2608a
quanxin <quanxin@pujitech.com>
2021-03-10 11:18:21 +0800

    重定向

--- src/components/global/content/content.tsx
+++ src/components/global/content/content.tsx
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from "react";
 import {observer} from "mobx-react";
 import style from "./content.module.less";
 import {SliderDataModel} from "@models/slider"
-import {Route, Redirect} from 'react-router';
-import {HomePage} from "@pages/homePage/homePage"
+import {Route, Switch, Redirect} from 'react-router';
+// import {HomePage} from "@pages/homePage/homePage"
 import {BasicPage} from "@pages/basicPage/basicPage"
 
 //  菜单组件
@@ -12,19 +12,21 @@ export const Content = observer(({store}) => {
 	//	console.log(routerListData.length);
 	return (
 		<main className={style.content}>
-			{
-				routerListData.map((routerItem: SliderDataModel) => {
-					return (
-						<Route key={routerItem.link}
-							   path={routerItem.link}
-						>
-                            <BasicPage routerItem={routerItem}/>
-						</Route>
-					)
-				})
-			}
-			<Route path='/index' exact={true} component={HomePage}/>
-			{/*{!!routerListData.length && <Redirect from='*' to='/index' exact={true}/>}*/}
+			<Switch>
+				{
+					routerListData.map((routerItem: SliderDataModel) => {
+						return (
+							<Route key={routerItem.link}
+								   path={routerItem.link}
+							>
+								<BasicPage routerItem={routerItem}/>
+							</Route>
+						)
+					})
+				}
+				{/*<Route path='/index' exact={true} component={HomePage}/>*/}
+				{!!routerListData.length && <Redirect from='*' to={routerListData[0].link} exact={true}/>}
+			</Switch>
 		</main>
 	)
 });
