e37a43a6e14c5b99c8f3ef2b3c310ef6e4f32508
quanxin <quanxin@pujitech.com>
2021-03-10 15:01:42 +0800

    api

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,31 +1,40 @@
 import axios, {Method} from 'axios';
 
 interface options {
-    method?: Method | undefined,
-    url: string,
-    params?: string,
-    data?: object,
+	method?: Method | undefined,
+	url: string,
+	params?: string,
+	data?: object,
 }
 
+axios.interceptors.request.use(function (config) {
+	const {data} = config;
+	if (data && data.isLoading) {
+		// console.log(config.data);
+	}
+	return config;
+});
+
 //  封装请求
-export async function request({
-                                  method = 'get',
-                                  url,
-                                  // params,
-                                  // data,
-                              }: options) {
-    return await axios({
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-        },
-        method,
-        url,
-        // params,
-        // data,
-    })
-        .then(v => v.data)
-        .catch(err => {
-            return Promise.reject(err);
-        });
+export async function request(
+	{
+		method = 'get',
+		url,
+		// params,
+		data,
+	}: options) {
+	return await axios({
+		headers: {
+			'Content-Type': 'application/json; charset=UTF-8',
+		},
+		method,
+		url,
+		// params,
+		data,
+	})
+		.then(v => v.data)
+		.catch(err => {
+			return Promise.reject(err);
+		});
 }
 
