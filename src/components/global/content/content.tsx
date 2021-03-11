d77f140a11b11c4c3e5887f60962cda0e0016e90
quanxin <quanxin@pujitech.com>
2021-03-11 19:07:44 +0800

    scroll

--- src/components/global/content/content.tsx
+++ src/components/global/content/content.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {observer} from "mobx-react";
 import style from "./content.module.less";
 import {SliderDataModel} from "@models/slider"
@@ -6,13 +6,33 @@ import {Route, Switch, Redirect} from 'react-router';
 // import {HomePage} from "@pages/homePage/homePage"
 import {BasicPage} from "@pages/basicPage/basicPage"
 import {RoutingGuard} from "@components/routingGuard/routingGuard";
+import {Store} from "@store/index";
 
 //  菜单组件
-export const Content = observer(({store}) => {
+export const Content = observer(({store}: { store: Store }) => {
 	const routerListData = store.getRouterListData;
-	//	console.log(routerListData.length);
+	// const [timer, setTimer] = useState(null);
+	const contentRef = useRef(null);
+	useEffect(() => {
+		const {current} = contentRef;
+		let timer: any = null;
+		const fn = (e) => {
+			if (timer) {
+				return;
+			}
+			timer = setTimeout(() => {
+				const {scrollTop} = e.target;
+				store.setScrollTop(scrollTop);
+				timer = null;
+			}, 100);
+		};
+		(current as any).addEventListener('scroll', fn, false);
+		return (() => {
+			(current as any).removeEventListener('scroll', fn, false);
+		});
+	}, []);
 	return (
-		<main className={style.content}>
+		<main className={style.content} ref={contentRef}>
 			<Switch>
 				{
 					routerListData.map((routerItem: SliderDataModel) => {
